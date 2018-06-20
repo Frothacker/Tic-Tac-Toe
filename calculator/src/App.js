ff13bc53db947a589461696ee1516959d8a38d56
Finn <finn.rothacker@gmail.com>
2018-06-20 08:42:41 +0800

    initial commit

---
+++ calculator/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-title">Create calculator here</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
